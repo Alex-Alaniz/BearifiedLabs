6860df41c1d6c49a91994083de35fd740f810844
Alex Alaniz <obyc@okaybearsyachtclub.io>
2023-10-02 13:53:31 -0400

    Production Deployment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -34,17 +34,17 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>BearifiedXYZ</title>
-        <meta name="description" content="A Web3 Agency: Powering dreams for Developers Artist & CreatorsStep into the NFT universe with us. We navigate the intricate maze of smart contracts and provide a robust Web3 drop platform. You bring the creativity, we handle the code. When our worlds collide, expect digital fireworks." />
+        <meta name="description" content="A Web3 Agency: Powering dreams for Developers Artist & Creators." />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://www.Bearified.xyz/" />
         <meta property="og:title" content="BearifiedXYZ" />
-        <meta property="og:description" content="A Web3 Agency: Powering dreams for Developers Artist & CreatorsStep into the NFT universe with us. We navigate the intricate maze of smart contracts and provide a robust Web3 drop platform. You bring the creativity, we handle the code. When our worlds collide, expect digital fireworks." />
+        <meta property="og:description" content="A Web3 Agency: Powering dreams for Developers Artist & Creators. Step into the NFT universe with us. We navigate the intricate maze of smart contracts and provide a robust Web3 drop platform. You bring the creativity, we handle the code. When our worlds collide, expect digital fireworks." />
         <meta property="og:image" content="images/BearifiedXYZ.png" />
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://www.Bearified.xyz/" />
         <meta property="twitter:title" content="BearifiedXYZ" />
-        <meta property="twitter:description" content="A Web3 Agency: Powering dreams for Developers Artist & CreatorsStep into the NFT universe with us. We navigate the intricate maze of smart contracts and provide a robust Web3 drop platform. You bring the creativity, we handle the code. When our worlds collide, expect digital fireworks." />
-        <meta property="twitter:image" content="images/BearifiedXYZ.png" />
+        <meta property="twitter:description" content="A Web3 Agency: Powering dreams for Developers Artist & Creators. Step into the NFT universe with us. We navigate the intricate maze of smart contracts and provide a robust Web3 drop platform. You bring the creativity, we handle the code. When our worlds collide, expect digital fireworks." />
+        <meta property="twitter:image" content="https://twitter.com/BearifiedXYZ/photo" />
       </Head>
       <nav className={styles.nav}>
         <ul className={styles.menu}>
